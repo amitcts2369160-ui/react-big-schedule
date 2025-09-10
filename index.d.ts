38736a3fae3b49378d8112828770671ce1938a4a
Amit Gupta <AGupt355@ITS.JNJ.com>
2025-09-10 15:46:57 +0530

    add types field and fallback type declarations

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export declare const Scheduler:any;
+export declare const SchedulerData:any;
+export declare const ViewType:any;
+export declare const DATE_FORMAT:any;
+export declare const View:any;
+export declare const EventItem:any;
+export declare const Resource:any;
